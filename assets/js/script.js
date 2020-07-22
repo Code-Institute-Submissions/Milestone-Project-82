0e28d6df21bdd3da005fe2c9ecb14e33033f44ec
James-Stewart110 <jamesstewart506@hotmail.co.uk>
2020-07-22 15:02:46 +0000

    beautified code

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,294 +16,298 @@ let win; /*has player won?*/
 let mute = false;
 
 /*----------------------------------------------------Constants--*/
-const buttonTop = document.querySelector('#button-top');
-const buttonRight = document.querySelector('#button-right');
-const buttonLeft = document.querySelector('#button-left');
-const buttonBottom = document.querySelector('#button-bottom');
-const turnCounter = document.querySelector('#counter');
-const strictButton = document.querySelector('#strict');
-const onButton = document.querySelector('#power');
-const startButton = document.querySelector('#start');
-const strictLight = document.getElementById('strictLight');
-const onLight = document.getElementById('onLight');
-const muteButton = document.getElementById('mute')
-const audio1 = document.getElementById('audio1');
-const audio2 = document.getElementById('audio2');
-const audio3 = document.getElementById('audio3');
-const audio4 = document.getElementById('audio4');
+const buttonTop = document.querySelector("#button-top");
+const buttonRight = document.querySelector("#button-right");
+const buttonLeft = document.querySelector("#button-left");
+const buttonBottom = document.querySelector("#button-bottom");
+const roundCounter = document.querySelector("#counter");
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#power");
+const startButton = document.querySelector("#start");
+const strictLight = document.getElementById("strictLight");
+const onLight = document.getElementById("onLight");
+const muteButton = document.getElementById("mute");
+const audio1 = document.getElementById("audio1");
+const audio2 = document.getElementById("audio2");
+const audio3 = document.getElementById("audio3");
+const audio4 = document.getElementById("audio4");
+
+/*-------------------------------------------Game and display on/off--*/
+onButton.addEventListener("click", (event) => {
+  if (on == false) {
+    on = true;
+    roundCounter.innerHTML = "-";
+    onLight.classList.add("lightOn");
+    flashColour();
+    setTimeout(() => {
+      clearColour();
+    }, 400);
+  } else if (on == true) {
+    on = false;
+    strict = false;
+    mute = false;
+    roundCounter.innerHTML = "";
+    onLight.classList.remove("lightOn");
+    onLight.classList.add("lightOff");
+    muteLight.classList.remove("lightOn");
+    strictLight.classList.remove("lightOn");
+    clearColour();
+    clearInterval(intervalId);
+  }
+});
 
 /*-------------------------------------------Strict mode on/off--*/
-strictButton.addEventListener('click', (event) => {
-    if (on == true && strict == false) {
-        strict = true;
-        strictLight.classList.add('lightOn');
-    } else {
-        strict = false;
-        strictLight.classList.remove('lightOn');
-        strictLight.classList.add('lightOff');
-    }
+strictButton.addEventListener("click", (event) => {
+  if (on == true && strict == false) {
+    strict = true;
+    strictLight.classList.add("lightOn");
+  } else {
+    strict = false;
+    strictLight.classList.remove("lightOn");
+    strictLight.classList.add("lightOff");
+  }
 });
 
 /*-------------------------------------------Mute on/off--*/
-muteButton.addEventListener('click', (event) => {
-    if (on == true && mute == false) {
-        mute = true;
-        muteLight.classList.add('lightOn');
-    } else {
-        mute = false;
-        muteLight.classList.remove('lightOn');
-        muteLight.classList.add('lightOff');
-    }
-})
-
-/*-------------------------------------------Game and display on/off--*/
-onButton.addEventListener('click', (event) => {
-    if (on == false) {
-        on = true;
-        turnCounter.innerHTML = '-';  
-        onLight.classList.add('lightOn');
-        flashColor();
-        setTimeout(() => {
-            clearColor()
-        }, 400)
-    } else if (on == true) {
-        on = false;
-        strict = false;
-        mute = false
-        turnCounter.innerHTML = '';
-        onLight.classList.remove('lightOn');
-        onLight.classList.add('lightOff');
-        muteLight.classList.remove('lightOn');
-        strictLight.classList.remove('lightOn');
-        clearColor();
-        clearInterval(intervalId);
-    }
+muteButton.addEventListener("click", (event) => {
+  if (on == true && mute == false) {
+    mute = true;
+    muteLight.classList.add("lightOn");
+  } else {
+    mute = false;
+    muteLight.classList.remove("lightOn");
+    muteLight.classList.add("lightOff");
+  }
 });
 
 /*-------------------------------------------Start button--*/
-startButton.addEventListener('click', (event) => {
-    if (on || win) {
-        play();
-    }
+startButton.addEventListener("click", (event) => {
+  if (on || win) {
+    play();
+  }
 });
 
 /*-------------------------------------------Start game function--*/
 function play() {
-    win = false;
-    order = [];
-    playerChoice = [];
-    flash = 0;
-    intervalId = 0;
-    turn = 1;
-    turnCounter.innerHTML = '1';
-    good = true;
-    for (var i = 0; i < 20; i++) {
-        order.push(Math.floor(Math.random() * 4) + 1);
-    }
-    compTurn = true;
-    intervalId = setInterval(gameTurn, 800);
+  win = false;
+  order = [];
+  playerChoice = [];
+  flash = 0;
+  intervalId = 0;
+  turn = 1;
+  roundCounter.innerHTML = "1";
+  good = true;
+  for (var i = 0; i < 20; i++) {
+    order.push(Math.floor(Math.random() * 4) + 1);
+  }
+  compTurn = true;
+  intervalId = setInterval(gameTurn, 800);
 }
 
-/*-------------------------------------------Checks who's turn it is--*/
+/*--------------------------------------Checks is player turn or comp turn-------*/
 function gameTurn() {
-    on = false;
+  on = false;
 
-    /*-------------------------------------------Computer's turn over--*/
-    if (flash == turn) {
-        clearInterval(intervalId);
-        compTurn = false;
-        clearColor();
-        on = true;
-    }
-    /*-------------------------------------------Computer's turn--*/
-    if (compTurn) {
-        on = false;
-        clearColor();
-        setTimeout(() => {               
-            if (order[flash] == 1) one();
-            if (order[flash] == 2) two();
-            if (order[flash] == 3) three();
-            if (order[flash] == 4) four();
-            flash++;
-        }, 200);
-    }
+  /*-------------------------------------------Computer's turn over--*/
+  if (flash == turn) {
+    clearInterval(intervalId);
+    compTurn = false;
+    clearColour();
+    on = true;
+  }
+
+  /*-------------------------------------------Computer's turn--*/
+  if (compTurn) {
+    on = false;
+    clearColour();
+    setTimeout(() => {
+      if (order[flash] == 1) one();
+      if (order[flash] == 2) two();
+      if (order[flash] == 3) three();
+      if (order[flash] == 4) four();
+      flash++;
+    }, 200);
+  }
 }
+
 
 /*------------------------------Changes buttons to lighter colour--*/
 function one() {
-    if (mute == false) {
-        audio1.play();
-    }
-    buttonTop.style.backgroundColor = '#ffffff';
+  if (mute == false) {
+    audio1.play();
+  }
+  buttonTop.style.backgroundColor = "#ffffff";
 }
 
 function two() {
-    if (mute == false) {
-        audio2.play();
-    }
-    buttonLeft.style.backgroundColor = '#ffffff';
+  if (mute == false) {
+    audio2.play();
+  }
+  buttonLeft.style.backgroundColor = "#ffffff";
 }
 
 function three() {
-    if (mute == false) {
-        audio3.play();
-    }
-    buttonRight.style.backgroundColor = '#ffffff';
+  if (mute == false) {
+    audio3.play();
+  }
+  buttonRight.style.backgroundColor = "#ffffff";
 }
 
 function four() {
-    if (mute == false) {
-        audio4.play();
-    }
-    buttonBottom.style.backgroundColor = '#ffffff';
+  if (mute == false) {
+    audio4.play();
+  }
+  buttonBottom.style.backgroundColor = "#ffffff";
 }
 
- /*------------------------------Returns button colour to normal--*/
-function clearColor() {
-    buttonTop.style.backgroundColor = '#cc2900';
-    buttonLeft.style.backgroundColor = '#00b300';
-    buttonRight.style.backgroundColor = '#0000cc';
-    buttonBottom.style.backgroundColor = '#ffcc00';
+/*------------------------------Returns button colour to normal--*/
+function clearColour() {
+  buttonTop.style.backgroundColor = "#cc2900";
+  buttonLeft.style.backgroundColor = "#00b300";
+  buttonRight.style.backgroundColor = "#0000cc";
+  buttonBottom.style.backgroundColor = "#ffcc00";
 }
 
 /*------------------------------Flashes buttons in order--*/
-function flashAllColors() {
-    buttonTop.style.backgroundColor = '#ffffff';
+function flashAllColours() {
+  buttonTop.style.backgroundColor = "#ffffff";
+  if (mute == false) {
+    audio1.play();
+  }
+  setTimeout(function () {
+    buttonLeft.style.backgroundColor = "#ffffff";
     if (mute == false) {
-        audio1.play();
+      audio2.play();
     }
-    setTimeout(function() {
-        buttonLeft.style.backgroundColor = '#ffffff';;
-        if (mute == false) {
-        audio2.play();
+  }, 300);
+  setTimeout(function () {
+    buttonBottom.style.backgroundColor = "#ffffff";
+    if (mute == false) {
+      audio4.play();
     }
-    }, 300);
-    setTimeout(function() {
-        buttonBottom.style.backgroundColor = '#ffffff';
-        if (mute == false) {
-        audio4.play();
+  }, 500);
+  setTimeout(function () {
+    buttonRight.style.backgroundColor = "#ffffff";
+    if (mute == false) {
+      audio3.play();
     }
-    }, 500);
-    setTimeout(function() {
-        buttonRight.style.backgroundColor = '#ffffff';
-        if (mute == false) {
-        audio3.play();
-    }
-    }, 700);
-    setTimeout(function() {
-        clearColor();
-    }, 1300);
+  }, 700);
+  setTimeout(function () {
+    clearColour();
+  }, 1300);
 }
 
 /*------------------------------Flashes all buttons--*/
 
-function flashColor() {
-    buttonTop.style.backgroundColor = '#ffffff';
-    buttonLeft.style.backgroundColor = '#ffffff';
-    buttonRight.style.backgroundColor = '#ffffff';
-    buttonBottom.style.backgroundColor = '#ffffff';
+function flashColour() {
+  buttonTop.style.backgroundColor = "#ffffff";
+  buttonLeft.style.backgroundColor = "#ffffff";
+  buttonRight.style.backgroundColor = "#ffffff";
+  buttonBottom.style.backgroundColor = "#ffffff";
 }
 
 /*--------------------------------------------------------Clickable buttons--*/
 
 /*------------------------------Calls the function to flash colours--*/
 
-buttonTop.addEventListener('click', (event) => {  /*Red*/
-    if (on == true) {
-        playerChoice.push(1);
-        one();
-        check();
-        noWin();
-    }
+buttonTop.addEventListener("click", (event) => {    /*Red*/
+  if (on == true) {
+    playerChoice.push(1);
+    one();
+    check();
+    noWin();
+  }
 });
 
-buttonLeft.addEventListener('click', (event) => {  /*Green*/
-    if (on == true) {
-        playerChoice.push(2);
-        two();
-        check();
-        noWin();
-    }
+buttonLeft.addEventListener("click", (event) => {       /*Green*/
+  if (on == true) {
+    playerChoice.push(2);
+    two();
+    check();
+    noWin();
+  }
 });
 
-buttonRight.addEventListener('click', (event) => {  /*Blue*/
-    if (on == true) {
-        playerChoice.push(3);
-        three();
-        check();
-        noWin();
-    }
+buttonRight.addEventListener("click", (event) => {      /*Blue*/
+  if (on == true) {
+    playerChoice.push(3);
+    three();
+    check();
+    noWin();
+  }
 });
 
-buttonBottom.addEventListener('click', (event) => {  /*Yellow*/
-    if (on == true) {
-        playerChoice.push(4);
-        four();
-        check();
-        noWin();    
-    }
+buttonBottom.addEventListener("click", (event) => {     /*Yellow*/
+  if (on == true) {
+    playerChoice.push(4);
+    four();
+    check();
+    noWin();
+  }
 });
 
 /*------------------------------Resets colours if game is not won--*/
 
 function noWin() {
-    if (!win) {
-            setTimeout(() => {
-                clearColor();
-            }, 300)
-    }
+  if (!win) {
+    setTimeout(() => {
+      clearColour();
+    }, 300);
+  }
 }
 
-/*------------Checks to see if player choice is wrong, if the game is won or if player choice is correct--*/
+/*------------Checks to see if last button the player clicked does not match the last button in the
+order, if the game is won or if player choice is correct--*/
 
 function check() {
-    if (playerChoice[playerChoice.length - 1] !== order[playerChoice.length - 1]) {
-        good = false;
-    }
-    if (playerChoice.length == 20 && good) {
-        flashAllColors();
-        winGame();
-    }
-    if (good == false) {
-        flashColor();
-        turnCounter.innerHTML = 'NO!';
-        setTimeout(() => {
-            turnCounter.innerHTML = turn;
-            clearColor()
+  if (
+    playerChoice[playerChoice.length - 1] !== order[playerChoice.length - 1]
+  ) {
+    good = false;
+  }
+  if (playerChoice.length == 20 && good) {
+    flashAllColours();
+    winGame();
+  }
+  if (good == false) {
+    flashColour();
+    roundCounter.innerHTML = "NO!";
+    setTimeout(() => {
+      roundCounter.innerHTML = turn;
+      clearColour();
 
-            if (strict) {
-                play();
-            } else {
-                resetPlayerTurn();
-                good = true;
-            }
-        }, 800)
-
-        noise = false;
-    }
-
-    if (turn == playerChoice.length && good && !win) {
-        turn++;
+      if (strict) {
+        play();
+      } else {
         resetPlayerTurn();
-        turnCounter.innerHTML = turn;
-    }
+        good = true;
+      }
+    }, 800);
+
+    noise = false;
+  }
+
+  if (turn == playerChoice.length && good && !win) {
+    turn++;
+    resetPlayerTurn();
+    roundCounter.innerHTML = turn;
+  }
 }
 
-/*------------------------------Game has been won! Restarts for another shot--*/
+/*------------------------------Resets player choice and makes it computers turn--*/
 
 function resetPlayerTurn() {
-    playerChoice = [];
-    compTurn = true;
-    flash = 0;
-    intervalId = setInterval(gameTurn, 800);
+  playerChoice = [];
+  compTurn = true;
+  flash = 0;
+  intervalId = setInterval(gameTurn, 800);
 }
 
 /*------------------------------Game has been won!--*/
 
 function winGame() {
-    flashColor();
-    turnCounter.innerHTML = 'WIN!';
-    on = false;
-    win = true;
+  flashColour();
+  on = false;
+  roundCounter.innerHTML = "WIN!";
+  win = true;
 }
-
